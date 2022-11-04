97d6e59d071fc69406a71b62370cb4e2bc787186
Tobias Hildebrandt <tobiash@tobiash.dev>
2022-11-03 22:15:23 -0400

    add basic client-server communication

--- src/client.rs
+++ src/client.rs
@@ -1,3 +1,13 @@
-fn main() {
-    println!("Hello, world!");
+use std::{net::{TcpStream, SocketAddr}, io::Write};
+
+fn main() -> Result<(), anyhow::Error> {
+    println!("starting client");
+
+    // connect to the server
+    let mut server_connection = TcpStream::connect("127.0.0.1:8888".parse::<SocketAddr>()?)?;
+
+    // send the server a message
+    server_connection.write(&"this is a message from the client!".as_bytes())?;
+
+    Ok(())
 }
--- src/server.rs
+++ src/server.rs
@@ -1,3 +1,26 @@
-fn main() {
-    println!("Hello, world!");
+use std::{net::{SocketAddr, TcpListener}, io::Read};
+
+fn main() -> Result<(), anyhow::Error> {
+    println!("starting server");
+
+    // create tcp socket bound to localhost:8888
+    let listen = TcpListener::bind("127.0.0.1:8888".parse::<SocketAddr>()?)?;
+
+    // incoming network buffer
+    let mut buffer = [0u8;2048];
+
+    // block until a client attempts to connect
+    let (mut client_conn, client_addr) = listen.accept()?;
+
+    println!("client connected from address: {}", client_addr);
+
+    // wait until the client sends us a message
+    client_conn.read(&mut buffer)?;
+
+    // decode bytes as utf-8
+    let client_message = std::str::from_utf8(&buffer)?;
+
+    println!("client sent: {}", client_message);
+
+    Ok(())
 }
